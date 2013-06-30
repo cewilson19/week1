4375762f15959f5030de8904c29feac2e170180f
Wilson <cewilson19@gmail.com>
2013-06-30 13:29:18 -0400

    calculater app

---
+++ calc.rb
@@ -0,0 +1,31 @@
+def say(msg)
+	puts "---- #{msg} ----"
+end
+
+
+say 'What\'s your first number?'
+num1 = gets.chomp
+
+say "The number was #{num1}"
+
+say 'And your second number?'
+num2 = gets.chomp
+
+say "The number was #{num2}"
+
+say "Would you like to 1) add 2) subtract 3) multiply 4) divide"
+operation = gets.chomp
+result = nil
+
+if operation == '1'
+	result = num1.to_i + num2.to_i
+elsif condition == '2'
+	result = num1.to_i - num2.to_i
+elsif condition == '3'
+	result = num1.to_i * num2.to_i
+elsif condition == '4'
+	result = num1.to_f / num2.to_f
+		
+end
+
+say "The answer is #{result}"
